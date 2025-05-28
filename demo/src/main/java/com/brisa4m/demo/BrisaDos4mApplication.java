f1f48c330a05e9a9c97fca3ef56ba3cf472715d7
João Pedro <jpdev777@gmail.com>
2025-05-28 15:03:01 -0300

    first commit

---
+++ demo/src/main/java/com/brisa4m/demo/BrisaDos4mApplication.java
@@ -0,0 +1,13 @@
+package com.brisa4m.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrisaDos4mApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrisaDos4mApplication.class, args);
+	}
+
+}
