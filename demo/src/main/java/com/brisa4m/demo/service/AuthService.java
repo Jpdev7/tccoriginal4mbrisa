d1690f16258b0db6fdf1585a9ae38b38a4560c37
PedroElorriaga <pedrohes2002@outlook.com>
2025-09-18 23:03:28 -0300

    feat: Regra de negocios de login com comentarios para ajudar

--- demo/src/main/java/com/brisa4m/demo/service/AuthService.java
+++ demo/src/main/java/com/brisa4m/demo/service/AuthService.java
@@ -1,7 +1,6 @@
 package com.brisa4m.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,32 +10,29 @@ import com.brisa4m.demo.models.UserModel;
 import com.brisa4m.demo.repositories.UserRepository;
 
 @Service
+// É como uma etiqueta dizendo: "Ei Spring, essa classe é um serviço, use-a para
+// lógica do sistema".
+// Em resumo: Service = "cérebro" da aplicação, onde mora a lógica.
 public class AuthService {
+    // Classe responsável pela regra de negócio do login.
     @Autowired
     UserRepository userRepository;
+    String errorMessage = "Usuário ou senha incorretos";
 
     public LoginResponse login(LoginRequest req) {
-        UserModel user = userRepository.findByEmail(req.getEmail())
-                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
-        System.out.print(checkPasswordHash(req.getPassword(), user.getPassword()));
-        if (!checkPasswordHash(req.getPassword(), user.getPassword()))
-            throw new RuntimeException("Senha incorreta");
-
-        return new LoginResponse("Usuario logado com sucesso!");
-    }
-
-    public LoginResponse createUser(LoginRequest req) {
-        UserModel user = userRepository.findByEmail(req.getEmail()).orElse(null);
-
-        if (user != null) {
-            throw new RuntimeException("Email ja existe");
+        UserModel user;
+        if (req.getEmail() != null) {
+            user = userRepository.findByEmail(req.getEmail())
+                    .orElseThrow(() -> new RuntimeException(errorMessage));
+        } else {
+            user = userRepository.findByCpf(req.getCpf())
+                    .orElseThrow(() -> new RuntimeException(errorMessage));
         }
 
-        req.setPassword(hashPassword(req.getPassword()));
-        UserModel newUser = new UserModel(req.getName(), req.getEmail(), req.getPassword());
-        userRepository.save(newUser);
+        if (!checkPasswordHash(req.getSenha(), user.getSenha()))
+            throw new RuntimeException(errorMessage);
 
-        return new LoginResponse("Usuario criado com sucesso!");
+        return new LoginResponse("Usuario logado com sucesso!");
     }
 
     private boolean checkPasswordHash(String normalPssword, String hashedPassword) {
@@ -44,12 +40,5 @@ public class AuthService {
         boolean passwordChecked = bc.matches(normalPssword, hashedPassword);
 
         return passwordChecked;
-    }
-
-    private String hashPassword(String normalPssword) {
-        String salt = BCrypt.gensalt();
-        String hashedPassword = BCrypt.hashpw(normalPssword, salt);
-
-        return hashedPassword;
     }
 }
